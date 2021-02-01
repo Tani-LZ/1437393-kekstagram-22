752aa48af25bcd9a1ba059799ddd162ded2c53d2
Tani-LZ <tanilaz@yandex.ru>
2021-02-01 19:32:22 +0200

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomInteger = function (min, max) {
   if (min || max < 0  || min >= max) {
-    alert('');
+    throw new Error('Минимальное число отрицательное или маинимальное число больше максимального');
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,10 +11,7 @@ getRandomInteger(0, 10);
 
 //Функция для проверки максимальной длины строки
 const getCommentLength = function (text, maxLength) {
-  if (text.length < maxLength) {
-    return true;
-  }
-  return false;
+  return text.length <= maxLength;
 };
 
 getCommentLength('Функция для проверки максимальной длины строки', 140)
