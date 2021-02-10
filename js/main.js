32d1861d74964fec79dded5663cbfb498c638e0e
Tani-LZ <tanilaz@yandex.ru>
2021-02-10 19:03:45 +0200

    Модуляция

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-import {IMAGE_DESCRIPTIONS, MESSAGES, NAMES} from './data.js';
 import {createComment, createImageDescription, createDescriptions} from './data.js';
 
-IMAGE_DESCRIPTIONS;
-MESSAGES;
-NAMES;
 createComment();
 createImageDescription();
 createDescriptions(25);
